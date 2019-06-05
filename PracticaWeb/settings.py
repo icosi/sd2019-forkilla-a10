b616c92b408b5b3823dcbca644d1334b836208e7
Joaquim Salvadó <quim@MacBook-Pro-de-Joaquim.local>
2019-06-05 03:16:21 +0200

    a

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -65,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'PracticaWeb.urls'
