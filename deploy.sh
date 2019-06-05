320c849a25af308006d9ee8ca05e6e813036f565
Joaquim Salvadó <quim@MacBook-Pro-de-Joaquim.local>
2019-06-05 20:46:16 +0200

    a

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 echo "---- git add ----"
 git add .
+echo ""
+echo "---- git commit ----"
 git commit -m"a"
+echo ""
+echo "---- git push ----"
 git push
+echo ""
+echo "---- git push heroku master ----"
 git push heroku master
